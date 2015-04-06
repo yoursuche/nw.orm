020bfde3a6a4ac015e823f4cb0f81b8a0898ebe1
kulgan <kulgan@3eadc2db-9f82-467c-a912-36acdd18a9b2>
2015-04-06 14:00:06 +0000

    h3 support
    git-svn-id: https://nimworks.svn.cloudforge.com/nworm@41 3eadc2db-9f82-467c-a912-36acdd18a9b2

---
+++ trunk/h3/neem/app/nw/orm/core/annotations/NwormTransaction.java
@@ -0,0 +1,14 @@
+package nw.orm.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NwormTransaction {
+
+	String configFile() default "hibernate.cfg.xml";
+
+}
